404fe4276c8aaa87fe756c288b27e609004b1f82
Lim Ting Wei <tingwei3931@gmail.com>
2024-11-11 00:07:08 +0800

    Remove key

--- sourceme.sh
+++ sourceme.sh
@@ -1,4 +1,4 @@
 LANGCHAIN_TRACING_V2=true
 LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
-LANGCHAIN_API_KEY="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"
+LANGCHAIN_API_KEY="YOUR_API_KEY"
 LANGCHAIN_PROJECT="pr-respectful-littleneck-81"
